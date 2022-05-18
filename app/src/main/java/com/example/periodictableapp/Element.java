3bc8a462a191746cbb4eb4f77eccfd2803988c40
3etimds <3etimds@bglab.local>
2022-05-18 08:11:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/periodictableapp/Element.java
@@ -0,0 +1,9 @@
+package com.example.periodictableapp;
+
+public class Element {
+    private int atomicNumber;
+    private String symbol;
+    private String name;
+    private String atimicMass;
+    private 
+}
---
+++ app/src/main/java/com/example/periodictableapp/NetworkUtils.java
@@ -0,0 +1,73 @@
+package com.example.periodictableapp;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetworkUtils {
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+    private static final String ELEMENTS_URL = "https://periodic-table-elements-info.herokuapp.com/";
+    private static final String ELEMENT_QUANTIDADE = "";
+    private static final String ELEMENT_METODO = "";
+    private static final String ELEMENT_PARAMETRO = "";
+
+
+    static String buscaElementos(String queryString, String metodo, String parametro) {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String bookJSONString = null;
+
+        try {
+            Uri builtURI = Uri.parse(ELEMENTS_URL).buildUpon()
+                    .appendQueryParameter(ELEMENT_QUANTIDADE, queryString)
+                    .appendQueryParameter(ELEMENT_METODO, metodo)
+                    .appendQueryParameter(ELEMENT_PARAMETRO, parametro)
+                    .build();
+
+            URL requestURL = new URL(builtURI.toString());
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+            InputStream inputStream = urlConnection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder builder = new StringBuilder();
+            String linha;
+
+            while ((linha = reader.readLine()) != null) {
+                builder.append(linha);
+                builder.append("\n");
+            }
+
+            if (builder.length() == 0) {
+                return null;
+            }
+
+            bookJSONString = builder.toString();
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Log.d(LOG_TAG, bookJSONString);
+        return bookJSONString;
+    }
+}
