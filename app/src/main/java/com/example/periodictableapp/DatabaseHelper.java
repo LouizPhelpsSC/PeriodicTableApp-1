8d15446060cf1bec2ea31fa346a476fd4222d774
3etimds <otaviopaulaferreira@gmail.com>
2022-05-18 08:35:24 -0300

    Iniciando o acesso à API

---
+++ app/src/main/java/com/example/periodictableapp/DatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.example.periodictableapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "db_periodictable.db";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "create table tb_Elements " +
+                        "(id integer primary key, name text,phone text,email text, street text,place text)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS tb_Elements");
+        onCreate(db);
+    }
+
+}
--- app/src/main/java/com/example/periodictableapp/Element.java
+++ app/src/main/java/com/example/periodictableapp/Element.java
@@ -1,9 +1,229 @@
 package com.example.periodictableapp;
 
+import java.util.Date;
+
 public class Element {
     private int atomicNumber;
     private String symbol;
     private String name;
-    private String atimicMass;
-    private 
+    private String atomicMass;
+    private String electronicConfiguration;
+    private double electronegativity;
+    private int atomicRadius;
+    private String ionRadius;
+    private String vanDerWaalsRadius;
+    private int ionizationEnergy;
+    private int electronAffinity;
+    private int oxidationStates;
+    private String standardState;
+    private String bondingType;
+    private int meltingPoint;
+    private int boilingPoint;
+    private double density;
+    private String groupBlock;
+    private Date yearDiscovered;
+    private String block;
+    private String cpkHexColor;
+    private int period;
+    private int group;
+    private boolean favorited;
+
+    // CONSTRUCTORS
+    public Element() {
+    }
+
+
+
+    // GETTERS AND SETTERS
+    public int getAtomicNumber() {
+        return atomicNumber;
+    }
+
+    public void setAtomicNumber(int atomicNumber) {
+        this.atomicNumber = atomicNumber;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAtomicMass() {
+        return atomicMass;
+    }
+
+    public void setAtomicMass(String atomicMass) {
+        this.atomicMass = atomicMass;
+    }
+
+    public String getElectronicConfiguration() {
+        return electronicConfiguration;
+    }
+
+    public void setElectronicConfiguration(String electronicConfiguration) {
+        this.electronicConfiguration = electronicConfiguration;
+    }
+
+    public double getElectronegativity() {
+        return electronegativity;
+    }
+
+    public void setElectronegativity(double electronegativity) {
+        this.electronegativity = electronegativity;
+    }
+
+    public int getAtomicRadius() {
+        return atomicRadius;
+    }
+
+    public void setAtomicRadius(int atomicRadius) {
+        this.atomicRadius = atomicRadius;
+    }
+
+    public String getIonRadius() {
+        return ionRadius;
+    }
+
+    public void setIonRadius(String ionRadius) {
+        this.ionRadius = ionRadius;
+    }
+
+    public String getVanDerWaalsRadius() {
+        return vanDerWaalsRadius;
+    }
+
+    public void setVanDerWaalsRadius(String vanDerWaalsRadius) {
+        this.vanDerWaalsRadius = vanDerWaalsRadius;
+    }
+
+    public int getIonizationEnergy() {
+        return ionizationEnergy;
+    }
+
+    public void setIonizationEnergy(int ionizationEnergy) {
+        this.ionizationEnergy = ionizationEnergy;
+    }
+
+    public int getElectronAffinity() {
+        return electronAffinity;
+    }
+
+    public void setElectronAffinity(int electronAffinity) {
+        this.electronAffinity = electronAffinity;
+    }
+
+    public int getOxidationStates() {
+        return oxidationStates;
+    }
+
+    public void setOxidationStates(int oxidationStates) {
+        this.oxidationStates = oxidationStates;
+    }
+
+    public String getStandardState() {
+        return standardState;
+    }
+
+    public void setStandardState(String standardState) {
+        this.standardState = standardState;
+    }
+
+    public String getBondingType() {
+        return bondingType;
+    }
+
+    public void setBondingType(String bondingType) {
+        this.bondingType = bondingType;
+    }
+
+    public int getMeltingPoint() {
+        return meltingPoint;
+    }
+
+    public void setMeltingPoint(int meltingPoint) {
+        this.meltingPoint = meltingPoint;
+    }
+
+    public int getBoilingPoint() {
+        return boilingPoint;
+    }
+
+    public void setBoilingPoint(int boilingPoint) {
+        this.boilingPoint = boilingPoint;
+    }
+
+    public double getDensity() {
+        return density;
+    }
+
+    public void setDensity(double density) {
+        this.density = density;
+    }
+
+    public String getGroupBlock() {
+        return groupBlock;
+    }
+
+    public void setGroupBlock(String groupBlock) {
+        this.groupBlock = groupBlock;
+    }
+
+    public Date getYearDiscovered() {
+        return yearDiscovered;
+    }
+
+    public void setYearDiscovered(Date yearDiscovered) {
+        this.yearDiscovered = yearDiscovered;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+
+    public String getCpkHexColor() {
+        return cpkHexColor;
+    }
+
+    public void setCpkHexColor(String cpkHexColor) {
+        this.cpkHexColor = cpkHexColor;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public boolean isFavorited() {
+        return favorited;
+    }
+
+    public void setFavorited(boolean favorited) {
+        this.favorited = favorited;
+    }
 }
